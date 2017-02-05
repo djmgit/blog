865115cf70fa86f064fbddd0e15b69648eee55d3
Deepjyoti Mondal <djmdeveloper060796@gmail.com>
2017-02-05 19:04:35 +0530

    Added the web app source files

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a blog created with ruby on rails.
+This my first rails web application :)
+
+Made it by following the tutorial present at http://guides.rubyonrails.org/v3.2/getting_started.html
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '89fab2947860a987e3f4556a04be830c9e6172b9e11fb9a9533c725de8237bb67bac3832e691b5df334c7a30b5e0778dda945a6e43012e45b78f77661cfe2cdb'
